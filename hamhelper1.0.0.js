3bde23ea9ddd1dc1627c5a1a680018ab403f6bc2
hamilsauce <jake@seahag.onmicrosoft.com>
2021-07-05 15:13:34 -0700

    updates

--- hamhelper1.0.0.js
+++ hamhelper1.0.0.js
@@ -1,61 +1,50 @@
-class HamHelper {
+export default class HamHelper {
 	constructor() {}
 
-	log(msg = '') {
+	static log(msg = '') {
 		console.log(msg)
 	}
 
 	// !!obj
-	isObjectEmpty(obj) {
+	static isObjectEmpty(obj) {
 		return Object.keys(obj).length === 0;
 	}
 
 	// !!queryselector
-	qs(selector, parentEl = document) {
+	static qs(selector, parentEl = document) {
+		if (typeof selector !== 'string') {
+			console.error('error: selector must be string (in HamHelper.qs()).');
+			return;
+		} else {
+			return parentEl.querySelector(selector)
+		}
+	}
+
+	//!! querySelectorAll
+	static qsa(selector, parentEl = document) {
 		if (typeof selector != 'string') {
-			console.log('err: selector must be string');
-			return
+			console.error('error: selector must be string (in HamHelper.qsa()).');
+			return;
 		} else {
-			const el = parentEl.querySelector(selector)
-			return el
+			return parentEl.querySelectorAll(selector);
 		}
 	}
 
-	//!!queryselectorAll
-	qsa(selector, parentEl = document) {
-		if (typeof selector != 'string') {
-			console.log('err: selector must be string');
-			return
-		} else {
-			const el = parentEl.querySelectorAll(selector)
-			return el
-		}
-	}
-
-	setElementDataset(el, dataObj = {}) {
-		if (!this.isObjectEmpty(dataObj)) {
-			Object.entries(dataObj)
-				.forEach(([prop, val]) => {
-					el.dataset[prop] = val
-				});
-		} else {
-			log('no data provided')
-		}
-	}
-
-	createNewElement(tag = 'div', id = '', classList = [], dataObj = {}) {
+	static createNewElement(tag = 'div', id = '', classList = [], dataObj = {}) {
 		const el = document.createElement(tag);
-		if (id) {
-			el.id = id
-		}
-		if (classList) {
-			el.classList.add(...classList)
-		}
+		if (id) el.id = id
+		if (classList) el.classList.add(...classList)
 		if (!this.isObjectEmpty(dataObj)) {
 			this.setElementDataset(el, dataObj)
 		}
 		return el;
 	}
+	
+	static setElementDataset(el, dataObj = {}) {
+		if (!this.isObjectEmpty(dataObj) && el) Object.entries(dataObj).forEach(([prop, val]) => el.dataset[prop] = val);
+		else this.log('no data provided')
+	}
+
 }
 
-export default new HamHelper();
+// export default new HamHelper();
--- helper.js
+++ helper.js
@@ -1,7 +1,18 @@
-import hamhelper from './hamhelper.js'
+import H from './hamhelper1.0.0.js'
+console.log('Ham');
+H.log('fuck')
 
-const output = hamhelper.qs('.output')
+const scriptTag = document.createElement('script')
+const newSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+const newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
 
-const myEl = hamhelper.createNewElement('div','my-el', ['fap', 'fap-two', 'fap-3'],{selected: false, roll: 'king'})
+console.log(scriptTag, newSvg.nodeName, newLine.nodeName);
+H.log(scriptTag.nodeName)
+
+
+const output = H.qs('.output')
+H.log(output.nodeName)
+
+const myEl = H.createNewElement('div','my-el', ['fap', 'fap-two', 'fap-3'],{selected: false, roll: 'king'})
 myEl.textContent = 'oooo'
 output.appendChild(myEl)
