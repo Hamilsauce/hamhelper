27d6bed0542a6e0b8729120a8df44dbabcf20ace
hamilsauce <jake@seahag.onmicrosoft.com>
2022-01-13 20:53:16 -0600

    b

--- modules/DOM.js
+++ modules/DOM.js
@@ -1,11 +1,11 @@
 export default {
-  
+
   // @ ADD REMOVE CLASS
   classList(el, add = true, ...classes) {
     if (typeof selector === 'string') return parentEl.querySelector(selector);
     else console.error('error: selector must be string (in HamHelper.qs()).');
   },
-  
+
   // @queryselector
   qs(selector, parentEl = document) {
     if (typeof selector === 'string') return parentEl.querySelector(selector);
@@ -17,12 +17,13 @@ export default {
     if (typeof selector === 'string') return parentEl.querySelectorAll(selector);
     else console.error('error: selector must be string (in HamHelper.qsa()).');
   },
-  
-  
+
+
 
   // @createElement
   newElement(tag = 'div', attrs = {}, children = [], template = '') {
-    const el = document.createElement(tag);
+
+    const el = attrs.namespaceURI ? document.createElementNS(attrs.namespaceURI, tag) : document.createElement(tag);
     el.innerHTML = template;
     for (let attr of Object.keys(attrs)) {
       if (attr === 'data') { Object.entries(attrs[attr]).forEach(([prop, val]) => el.dataset[prop] = val) }
