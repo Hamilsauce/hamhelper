f4eed0a1b7a053865b9112426f7f0c70d612de11
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-05-09 11:56:54 -0500

    i

--- draggable.js
+++ draggable.js
@@ -4,10 +4,15 @@
    element with the `detail` property of the event carrying XY
    coordinates for the location of the element.
 */
-let timer = 0
+
 export const draggable = (parent, element) => {
-  let svg = parent
-  let el = element //.querySelector('.slider-el');
+  let svg = parent;
+  let el = element;
+
+  if (!(svg instanceof SVGElement && el instanceof Element)) {
+    console.error('Draggable parent not an SVG element or target not an element');
+    return;
+  };
 
   while (svg && svg.tagName != 'svg') svg = svg.parentNode;
   let pt = svg.createSVGPoint();
@@ -20,26 +25,22 @@ export const draggable = (parent, element) => {
   el.addEventListener('pointerdown', startMove, false);
 
   function startMove(evt) {
-    
-    // We listen for pointermove/up on the root-most
-    // element in case the pointer is not over svg.
-    // svg.addEventListener('pointermove', elMove, false);
-    // svg.addEventListener('pointerup', finishMove, false);
 
-    // Ensure that the first transform is a translate()
     xlate = xforms.numberOfItems > 0 && xforms.getItem(0);
     if (!xlate || xlate.type != SVGTransform.SVG_TRANSFORM_TRANSLATE) {
       xlate = xforms.createSVGTransformFromMatrix(svg.createSVGMatrix());
       xforms.insertItemBefore(xlate, 0);
     }
-    
+
     txStartX = xlate.matrix.e;
     txStartY = xlate.matrix.f;
+
     pointerStart = inElementSpace(evt);
+
     fireEvent('dragstart');
+
     el.addEventListener('pointermove', elMove, true);
     el.addEventListener('pointerup', finishMove, true);
-
   }
 
   function elMove(evt) {
@@ -48,11 +49,11 @@ export const draggable = (parent, element) => {
       txStartX + point.x - pointerStart.x,
       txStartY + point.y - pointerStart.y
     );
+
     fireEvent('drag');
   }
 
   function finishMove(evt) {
-    
     svg.removeEventListener('pointerup', finishMove, false);
     svg.removeEventListener('pointermove', elMove, false);
     fireEvent('dragend');
@@ -60,7 +61,7 @@ export const draggable = (parent, element) => {
 
   function fireEvent(eventName) {
     const event = new Event(eventName);
-    event.detail = { x: xlate.matrix.e, y: xlate.matrix.f };
+    event.detail = { x: xlate.matrix.e, y: xlate.matrix.f, target: el };
     return svg.dispatchEvent(event);
   }
 
@@ -69,5 +70,12 @@ export const draggable = (parent, element) => {
     pt.x = evt.clientX;
     pt.y = evt.clientY;
     return pt.matrixTransform(svg.getScreenCTM().inverse());
+  }
+
+  return () => {
+    el.removeEventListener('pointerdown', startMove, false);
+    el.removeEventListener('pointermove', elMove, true);
+    el.removeEventListener('pointerup', finishMove, true);
+    fireEvent('removedrag');
   }
 }
