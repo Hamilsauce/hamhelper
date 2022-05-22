d35f79ce53dcde321156fdfac8e364ea9f3fed68
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-05-22 02:15:29 -0500

    eventer

---
+++ event-emitter.js
@@ -0,0 +1,52 @@
+/*
+  eventRegistry: A Map Object keyed with event names
+    each entry being a Set Object containing
+    listener callbacks
+*/
+
+export class EventEmitter extends EventTarget {
+#eventRegistry
+  constructor() {
+    super();
+    this.#eventRegistry = new Map()
+    this.listeners = [];
+  }
+
+  registerEvent(eventName) {
+    if (this.#eventRegistry.has(eventName)) return this.#eventRegistry.get(eventName)
+    return this.#eventRegistry
+      .set(eventName, new Set())
+      .get(eventName)
+
+  }
+  registerListener(eventName, listener) {
+    this.registerEvent(eventName).add(listener);
+    return this
+  }
+
+  on(type, listener) {
+    this.registerListener(type, listener);
+    return () => this.unregisterListener(type, listener)
+  }
+
+  unregisterListener(eventName, listener) {
+    return this.#eventRegistry.get(eventName).delete(listener)
+  }
+
+  off(type, listener) {
+    this.removeEventListener(type, listener)
+  }
+
+  emit(evt, data) {
+    if (!this.#eventRegistry.has(evt)) return;
+    this.#eventRegistry.get(evt).forEach(_ => _(data))
+  }
+  
+  dispatch(name, detail = {}) {
+    this.self.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
+
+    this.listeners.forEach((l, i) => {
+      l.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
+    });
+  }
+}
