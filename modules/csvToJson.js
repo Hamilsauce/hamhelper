08c25fbdfc17263bb1019f9d52573bb1d2734e1d
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-05-26 03:58:43 -0500

    u

--- modules/csvToJson.js
+++ modules/csvToJson.js
@@ -1,95 +1,24 @@
-// log last updated: 9.27.21 - refactored some for each loops into a map and reduce
 
-const mapDelimiter = delimiter => {
-  const del = delimiter.toLowerCase();
-  let delim;
-  if (del === ',') return ','
+export const csvToJson = (raw) => {
+  let lines = raw.split(/(?:\r\n|\n)+/).filter(function(el) { return el.length != 0 });
+  let headers = lines.splice(0, 1)[0].split(",");
+  let valuesRegExp = /(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g;
+  let elements = [];
+  let matches;
 
+  for (let i = 0; i < lines.length; i++) {
+    let element = {};
+    let j = 0;
 
-  if (del === 'tab') {
-    delim = '\t';
-  } else if (del === 'space') {
-    delim = ' ';
-  } else if (del === 'comma') {
-    delim = ',';
-  } else {
-    delim = -1; // error if not matched
-  }
-  return delim;
-}
+    while (matches = valuesRegExp.exec(lines[i])) {
+      var value = matches[1] || matches[2];
+      value = value.replace(/\"\"/g, "\"");
 
-//* convertToObjects: does all the record-by-record row-to-obj work; output: array of records as objects
-const convertToObjects = (colNames, dataBody, delim) => {
-  let recordsAsObjects = dataBody
-    .map(row => {
-      let i = 0;
-
-      const recordObj = row.split(delim)
-        .reduce((record, recordFields) => {
-          record[colNames[i]] = !recordFields ? null : recordFields;
-          ++i;
-          return record;
-        }, {});
-      return recordObj;
-    });
-  return recordsAsObjects;
-}
-
-//* csvToJson: the module, provides interface/API layer for external use
-const csvToJson = (input, delimiter = 'comma') => {
-  const csvToObjs = source => { //organizing function, maps API inputs with various function params
-    let err = '';
-    const delim = mapDelimiter(delimiter);
-
-    if (delim === -1) {
-      err = 'Error: invalid delimiter provided.';
-      console.error(err);
-      return err;
-    } else {
-      let dataBody = source.split('\n');
-      const colNames = dataBody.shift().trim().split(delim);
-
-      return convertToObjects(colNames, dataBody, delim);
-    }
-  }
-  return csvToObjs(input);
-}
-
-export default {
-
-  csvToJson(input, delimiter = 'comma') {
-    const csvToObjs = source => { //organizing function, maps API inputs with various function params
-      let err = '';
-      const delim = mapDelimiter(delimiter);
-
-      if (delim === -1) {
-        err = 'Error: invalid delimiter provided.';
-        console.error(err);
-        return err;
-      } else {
-        let dataBody = source.split('\n');
-        const colNames = dataBody.shift().trim().split(delim);
-
-        return convertToObjects(colNames, dataBody, delim);
-      }
+      element[headers[j]] = value;
+      j++;
     }
 
-
-    return csvToObjs(input);
-  },
-
-  getColumns(csv) {
-    return csv.substring(0, csv.indexOf('\n')).trim().split(',')
-  },
-
-
-  help(method = null) {
-    return `
-      
-        === CSV TO JSON ===
-    
-  - getColumnNames(csv) => string[]
-  - csvToJson(input, delimiter = 'comma') => json
-      `.trim();
+    elements.push(element);
   }
+  return elements;
 }
