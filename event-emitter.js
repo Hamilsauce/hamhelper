f8a41be14d06398811a8b6e3e57d62d366f41db6
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-11-06 10:55:33 -0600

    f

--- event-emitter.js
+++ event-emitter.js
@@ -41,9 +41,77 @@ export class EventEmitter extends EventTarget {
     this.removeEventListener(type, listener)
   }
 
+  fire(listener, data) {
+    listener(data);
+    return true
+  }
+
   emit(evt, data) {
     if (!this.#eventRegistry.has(evt)) return;
-    this.#eventRegistry.get(evt).forEach(_ => _(data))
+    this.#eventRegistry.get(evt).forEach(_ => this.fire(_, data))
+  }
+
+  dispatch(name, detail = {}) {
+    this.self.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
+
+    this.listeners.forEach((l, i) => {
+      l.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
+    });
+  }
+}
+
+export class StateEmitter extends EventEmitter {
+  #cache = {};
+
+  constructor(state) {
+    super();
+
+  }
+
+  cacheEventValue(eventName, value) {
+    this.#cache[eventName] = values;
+    return this.#cache[eventName];
+  }
+
+  registerEvent(eventName) {
+    if (this.#eventRegistry.has(eventName)) return this.#eventRegistry.get(eventName);
+
+    return this.#eventRegistry
+      .set(eventName, new Set())
+      .get(eventName);
+  }
+
+  // fire() {
+  //   super.fire()
+  // }
+
+  registerListener(eventName, listener) {
+    super.registerEvent(eventName).add(listener);
+    
+    if (!!this.#cache[eventName]) {
+      this.fire(listener, this.#cache[eventName])
+    }
+    return this
+  }
+
+  on(type, listener) {
+    this.registerListener(type, listener);
+    return () => this.unregisterListener(type, listener)
+  }
+
+  unregisterListener(eventName, listener) {
+    return this.#eventRegistry.get(eventName).delete(listener)
+  }
+
+  off(type, listener) {
+    this.removeEventListener(type, listener)
+  }
+
+  emit(eventName, data) {
+    if (!this.#eventRegistry.has(eventName)) return;
+    this.#eventRegistry.get(eventName).forEach(_ => this.fire(_, data));
+    
+    this.#cache[eventName] = data
   }
 
   dispatch(name, detail = {}) {
