d3604168df08ff75b7144d3d609908d1a4cd034b
hamilsauce <jacob.w.hamilton@outlook.com>
2025-07-21 17:39:10 -0500

    update event-emitter.js

--- event-emitter.js
+++ event-emitter.js
@@ -7,56 +7,48 @@
 export class EventEmitter extends EventTarget {
   #eventRegistry = new Map();
   #listeners = [];
-
+  
   constructor() {
     super();
   }
-
+  
   #registerEvent(eventName) {
     if (this.#eventRegistry.has(eventName)) return this.#eventRegistry.get(eventName);
-
+    
     return this.#eventRegistry
       .set(eventName, new Set())
       .get(eventName);
-
+    
   }
-
+  
   #registerListener(eventName, listener) {
     this.#registerEvent(eventName).add(listener);
     return this
   }
-
+  
   on(type, listener) {
     this.#registerListener(type, listener);
     return () => this.#unregisterListener(type, listener)
   }
-
+  
   #unregisterListener(eventName, listener) {
     return this.#eventRegistry.get(eventName).delete(listener)
   }
-
+  
   off(type, listener) {
-    this.removeEventListener(type, listener)
+    this.#unregisterListener(type, listener);
+    this.removeEventListener(type, listener);
   }
-
-  fire(listener, data) {
-    listener(data);
-
-    return true;
-  }
-
+  
   emit(evt, data) {
     if (!this.#eventRegistry.has(evt)) return;
-    this.#eventRegistry.get(evt).forEach(_ => this.fire.bind(this)(_, data))
+    const listeners = [...(this.#eventRegistry.get(evt) ?? [])];
+    const globalListeners = [...(this.#eventRegistry.get('*') ?? [])];
+    
+    for (const listener of [...listeners, ...globalListeners]) {
+      listener(data, evt);
+    }
   }
-
-  // dispatch(name, detail = {}) {
-  //   this.self.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
-
-  //   this.#listeners.forEach((l, i) => {
-  //     l.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
-  //   });
-  // }
 }
 
 // export class StateEmitter extends EventEmitter {#
