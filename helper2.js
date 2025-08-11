52d523ea02f700469b49b95386393e111c0b54ef
hamilsauce <jacob.w.hamilton@outlook.com>
2025-08-11 16:12:18 -0500

    update

--- helper2.js
+++ helper2.js
@@ -1,20 +1,29 @@
 import ham from 'https://hamilsauce.github.io/hamhelper/hamhelper1.0.0.js';
 const { waitMs, rx, DOM, TwoWayMap, event, date, array, utils, pipeline, text, help } = ham;
 import ham2 from './hamhelper1.0.0.js'
-const {addSvgPinchZoom} = ham2
+const { addSvgPinchZoom } = ham2
 // console.time('timer1')
 import './_two-pointer-search.js';
 // console.warn('waitMs', waitMs)
 // waitMs()
 // import H from './hamhelper1.0.0.js'
 import { pipe } from './modules/pipeline.js';
+import { EventEmitter } from './event-emitter.js';
 // import { TwoWayMap } from './modules/TwoWayMap.js';
 // import rx from './modules/rxjs.js'
 // console.log('H.help()', H.help())
 
 // const { DOM,TwoWayMap, event, date, array, utils, pipeline, text, help } = H;
 // console.log({ pipeline });
+class MyClass extends EventEmitter {
+  constructor() {
+    super()
+  };
+  get prop() { return this._prop };
+  set prop(newValue) { this._prop = newValue };
+}
 
+const myClass = new myClass();
 const poop = 'kebab-case'
 // console.log('poop.split(/^[A-Z]*$/)', poop.split(/[A-Z]/g)) 
 // console.log('camelToKebab', text.camelToKebab(poop));
