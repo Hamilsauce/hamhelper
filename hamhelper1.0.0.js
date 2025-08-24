07300e48584679dbcc59b6daa8c742f27ad7b72c
hamilsauce <jacob.w.hamilton@outlook.com>
2025-08-23 19:01:26 -0500

    update

--- hamhelper1.0.0.js
+++ hamhelper1.0.0.js
@@ -6,7 +6,6 @@ import {
   date,
   array,
   text,
-  event,
   helpObj,
   utils,
   c2j,
@@ -28,44 +27,44 @@ import {
 } from './modules/index.js';
 
 export default class HamHelper {
-
+  
   /* @ waitMs */
   static get waitMs() {
     return waitMs
   }
-
+  
   /* @ sleep */
   static get sleep() {
     return sleep
   }
-
+  
   /* @ addSvgPinchZoom */
   static get addSvgPinchZoom() {
     return addSvgPinchZoom
   }
-
+  
   /* @ addPanAction    */
   static get addPanAction() {
     return addPanAction
   }
-
+  
   /* @ getPanZoom    */
   static get getPanZoom() {
     return getPanZoom
   }
-
+  
   /* @ PIPELINE */
   static get pipeline() { return pipe }
-
+  
   /* @ IMPORT UMD MODULE EXPORTS */
   static get importUMD() { return importUMD }
-
+  
   /* @ RXJS LIBRARY MODULE */
   static get rxjs() { return rxjs }
-
+  
   /* @ TIME LOGGER MODULE */
   static get timer() { return timer }
-
+  
   /* @ CONSTANTS */
   static get CONSTANTS() {
     return {
@@ -75,49 +74,46 @@ export default class HamHelper {
       ).map(x => String.fromCharCode(x)),
     }
   }
-
+  
   /* @ JSON */
   static get json() { return json }
-
+  
   /* @ RXJS addDragAction */
   static get addDragAction() { return addDragAction }
-
+  
   /* @ Templater */
   static get template() { return templater }
-
+  
   /* @ object */
   static get object() { return object }
-
+  
   /* @ log */
   static get log() { return log }
-
+  
   /* @ TwoWayMap */
   static get TwoWayMap() { return twoWayMap }
-
+  
   /* @ CSVTOJSON */
   static get csvToJson() { return c2j }
-
+  
   /* @ DOM */
   static get DOM() { return dom }
-
+  
   /* @ SVG */
   static get SVG() { return svg }
-
+  
   /* TODO ERROR    @ TEXT */
   static get text() { return text }
-
+  
   /* @ DATE */
   static get date() { return date }
-
+  
   /* @ ARRAY */
   static get array() { return array }
-
-  /* @ EVENT */
-  static get event() { return event }
-
+  
   /* @ UTIL */
   static get utils() { return utils }
-
+  
   /* @ DOWNLOAD */
   static download(filename, text) {
     const element = document.createElement('a');
@@ -128,17 +124,17 @@ export default class HamHelper {
     element.click();
     document.body.removeChild(element);
   }
-
-
+  
+  
   /* @ HELP */
   static get help() {
     const { help } = helpObj;
     return help;
   }
-
+  
   /* @ MAP */
   static mapFromObject(obj) { return new Map(Object.entries(obj)) }
-
+  
   /* @ ASYNC */
   static async asyncFetchMap(sources) {
     // console.log(sources);
@@ -148,7 +144,7 @@ export default class HamHelper {
         return { key: name, data: await response.json() }
       }))
   };
-
+  
   /* @ ASYNC */
   static async asyncReduce({ collectionName, sources }) {
     return (await fetchCollectionSources(sources))
@@ -164,10 +160,10 @@ export default class HamHelper {
         [collectionName, new Map()]
       ]));
   };
-
+  
   /* @ OBJECT */
   static isObjectEmpty(obj) { return Object.keys(obj).length === 0 }
-
+  
   /* @ OBJECT */
   static arrayFromObjectProperties(obj, propName = 'propertyName') {
     return Object.entries(obj)
@@ -176,7 +172,7 @@ export default class HamHelper {
         return [...acc, { ...value }]
       }, [])
   }
-
+  
   getValueType(target, deepTest = false, customClassType = null) {
     // TODO Work on params
     if (target === null) return 'null';
@@ -198,7 +194,7 @@ function elt(name, attrs, ...children) {
     dom.setAttribute(attr, attrs[attr]);
   }
   for (let child of children) {
-
+    
     dom.appendChild(child);
   }
   return dom;
--- modules/index.js
+++ modules/index.js
@@ -3,7 +3,7 @@ export { default as dom } from './DOM.js';
 export { default as date } from './date.js';
 export { default as array } from './array.js';
 export { default as text } from './text.js'; // <<< ERROR
-export { default as event } from './event.js';
+// export { default as event } from './event.js';
 export { default as helpObj } from './help.js';
 export { default as utils } from './utils.js';
 export { default as c2j } from './csvToJson.js';
